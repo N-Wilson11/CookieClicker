d0af02f1f542fc423451d98656b0d58aa226cf30
Nathan Wilson <nathankishawn@gmail.com>
2025-02-07 14:23:11 +0100

    Added a cookie clicker and made another empty view

---
+++ CreditsView.swift
@@ -0,0 +1,18 @@
+//
+//  CreditsView.swift
+//  MBDI_WS2_CookieClicker
+//
+//  Created by Nathan Wilson on 07/02/2025.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreditsView()
+}
